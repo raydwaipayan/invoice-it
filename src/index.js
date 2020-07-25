4996a90aaf869b3b6bcf7b84496c812d981e73c7
Dwaipayan Ray <dwaipayanray1@gmail.com>
2020-07-26 00:47:48 +0530

    Added emitter website

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ generator.configure({
   global: {
     logo: 'http://placehold.it/230x70&text=logo',
     order_reference_pattern: '$prefix{OR}$date{YYMM}$separator{-}$id{00000}',
-    invoice_reference_pattern: '$prefix{IN}$date{YYMM}$separator{-}$id{00000}',
+    invoice_reference_pattern: '$prefix{IN}$date{YYYY}$separator{-}$id{00000}',
     order_template: `${__dirname}/../static/order.pug`,
     order_note: '',
     invoice_template: `${__dirname}/../static/invoice.pug`,
